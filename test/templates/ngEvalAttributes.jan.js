8cdc30b12fb872fa063cf21f686b27f47bac915f
Christian Allen <christian@christianallen.com>
2014-05-06 08:03:49 -0400

    adding unit tests for evalAttributes

---
+++ test/templates/ngEvalAttributes.jan.js
@@ -0,0 +1,13 @@
+// var evalAttributes = ['href', 'src', 'title', 'placeholder', 'popover', 'value', 'alt'];
+module.exports = function(model) {
+    return [
+        span({'eval-id': 'bar'}),
+        span({'eval-href': 'foo'}),
+        span({'eval-src': 'bar'}),
+        span({'eval-title': 'foo'}),
+        span({'eval-placeholder': 'bar'}),
+        span({'eval-popover': 'foo'}),
+        span({'eval-value': 'bar'}),
+        span({'eval-alt': 'foo'})
+    ];
+};
